dd8a0e8ffbd7e4d596b94c224d8ad5e645df0fac
Quentin ROUET <qrouet@campus-eni.fr>
2024-06-12 12:19:47 +0200

    INIT

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonInput,
+  IonSelect,
+  IonSelectOption
+} from '@ionic/angular/standalone';
+import {FormsModule} from "@angular/forms";
+import {NgIf} from "@angular/common";
+import {IonicModule} from "@ionic/angular";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonicModule, FormsModule, NgIf],
 })
 export class HomePage {
+  pseudo = '';
+  difficulty = 'easy';
+  error? : string;
+  memorize: boolean = false;
+  isGameStarted = false;
+  responseSubmitted = false;
+  userResponse? :string;
+
+  startGame(){
+    this.isGameStarted = true;
+  }
+  setUserResponse(response : string){
+    this.responseSubmitted=true;
+    this.userResponse = response;
+  }
   constructor() {}
 }
